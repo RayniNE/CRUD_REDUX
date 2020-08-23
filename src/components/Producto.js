da2d3569fc40f79dde8ac132d341403ba489fae5
Rayni Nuñez <timetopwm@gmail.com>
2020-08-23 15:28:53 -0400

    Commit con proyecto ya terminado.

---
+++ src/components/Producto.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
+//Redux
+import { useDispatch } from 'react-redux';
+import { borrarProductoAction, obtenerProductoEditar } from '../actions/productoActions';
+
+const Producto = ({ producto }) => {
+
+    const { nombre, precio, id } = producto;
+
+    const dispatch = useDispatch();
+    const history = useHistory(); //Habilitar history para la redireccion.
+
+    //Cuando si el usuario desea eliminar.
+    const confirmarEliminarProducto = id => {
+        //Preguntar al usuario.
+        Swal.fire({
+            title: 'Estas seguro?',
+            text: 'Un producot que se elimina no se puede recuperrr',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                //Se pasa al action.
+                dispatch(borrarProductoAction(id));
+            }
+        });
+
+    }
+
+    //Funcion que redirige de forma programada.
+    const redireccionarEdicion = (producto) => {
+        dispatch(obtenerProductoEditar(producto));
+        history.push(`/productos/editar/${producto.id}`)
+    }
+
+    return (
+        <tr>
+            <td> {nombre} </td>
+            <td> <span className="font-weight-bold"> $ {precio} </span> </td>
+            <td className="acciones">
+                <button type="button" className="btn btn-primary mr-2" onClick={() => redireccionarEdicion(producto)}>
+                    Editar
+                </button>
+
+                <button type="button" className="btn btn-danger" onClick={() => confirmarEliminarProducto(id)}> Eliminar </button>
+            </td>
+        </tr>
+    );
+}
+
+export default Producto;
